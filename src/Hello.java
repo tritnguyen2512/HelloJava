2b5f9803699b2e603093ff9e7423cdc64ed12f5f
tritnguyen2512 <thanh_tri@yahoo.com>
2020-07-24 14:03:29 -0700

    Hello Java
    First Java Program

---
+++ src/Hello.java
@@ -0,0 +1,15 @@
+
+class ThrowEx
+{
+    
+}
+public class Hello
+{
+    public static void main(String args[])
+    {
+
+        String str = null;
+        System.out.println(str.length());
+
+    }
+}
